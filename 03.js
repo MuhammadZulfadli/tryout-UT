6fa3139b36af1a88a9f7417c6557b132befa8b74
Muhammad Zulfadli <zulfadlisimatupang48@gmail.com>
2020-06-30 20:12:01 +0700

    revisi fix

--- 03.js
+++ 03.js
@@ -19,7 +19,9 @@ const average = () => {
     for (let k = 0; k < angka.length; k++) {
         total += angka[k];
     }
-    console.log('Nilai rata - rata ' + total / angka.length)
+    let a = total / angka.length
+    let result = Number.parseInt(a)
+    console.log(`Nilai rata-rata ${result}`)
 }
 
 max()
@@ -27,18 +29,3 @@ min()
 average()
 
 
-// let angka = []
-// for (let i = 0; i <= 100; i++) {
-//     angka.push(Math.floor(Math.random() * 100) + 1)
-// }
-
-// let total = 0;
-// for (let j = 0; j < angka.length; j++) {
-//     total += angka[j]
-// }
-
-// const average = total / angka.length
-
-// console.log('Nilai maksimum ' + Math.max(...angka))
-// console.log('Nilai minimum ' + Math.min(...angka))
-// console.log('Nilai rata - rata ' + average)
